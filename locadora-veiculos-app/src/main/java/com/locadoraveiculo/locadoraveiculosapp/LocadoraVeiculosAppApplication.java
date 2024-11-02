dfdff931d7e76079131ff7b0516bc881fde2ca33
Thiagoqdev <Thiagoq.dev@gmail.com>
2024-11-02 12:56:53 -0300

    Estrutura do projeto

---
+++ locadora-veiculos-app/src/main/java/com/locadoraveiculo/locadoraveiculosapp/LocadoraVeiculosAppApplication.java
@@ -0,0 +1,13 @@
+package com.locadoraveiculo.locadoraveiculosapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocadoraVeiculosAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocadoraVeiculosAppApplication.class, args);
+	}
+
+}
